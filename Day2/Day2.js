6bb9eeee10d33b13dc92222232fe2910f5205ce5
Bruno Machado <bruno.sabbagmachado@outlook.com>
2022-03-24 09:34:25 -0300

    Desafio dia 2

---
+++ Day2/Day2.js
@@ -0,0 +1,5 @@
+var nomeUsuario = prompt("Qual o seu nome?");
+var idadeUsuario = prompt("Quantos anos você tem?");
+var linguagemUsuario = prompt("Qual linguagem de programação você está estudando?");
+
+document.write("Olá " + nomeUsuario + ", você tem " + idadeUsuario + " anos e já está aprendendo " + linguagemUsuario + ".");
