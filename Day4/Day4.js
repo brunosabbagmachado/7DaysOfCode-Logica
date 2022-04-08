8c1970777052d39ee29b4989922757d6144871a5
Bruno Machado <bruno.sabbagmachado@outlook.com>
2022-04-07 22:43:30 -0300

    atualizando dia 4

--- Day4/Day4.js
+++ Day4/Day4.js
@@ -3,3 +3,12 @@ var minimo = 0;
 var numeroSortido = Math.floor(Math.random() * (maximo - minimo + 1) + minimo);
 
 console.log(numeroSortido);
+
+var pergunta = prompt("Digite um numero de 0 a 10");
+
+
+if (numeroSortido == pergunta) {
+    document.write("acertou");    
+} else {
+    document.write("errou");
+}
