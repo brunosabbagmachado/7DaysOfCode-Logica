da04b03b6a7a6ae31f752b1027615eec02f63b01
Bruno S. Machado <bruno.sabbagmachado@outlook.com>
2022-05-04 21:34:33 -0300

    retomando projeto

--- Day4/Day4.js
+++ Day4/Day4.js
@@ -4,11 +4,11 @@ var numeroSortido = Math.floor(Math.random() * (maximo - minimo + 1) + minimo);
 
 console.log(numeroSortido);
 
-var pergunta = prompt("Digite um numero de 0 a 10");
+var pergunta = prompt("Digite um numero de 0 a 10 para descobrir o número secreto");
 
 
 if (numeroSortido == pergunta) {
-    document.write("acertou");    
+    document.write("Acertou");    
 } else {
-    document.write("errou");
+    document.write("Errou");
 }
